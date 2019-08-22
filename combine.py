66f91cc1ab7b51d80c5c35799b7cb67553dae21a
ririgi <lrpbaltazar@gmail.com>
2019-08-22 11:36:54 +0800

    add code for combining of rows per month

---
+++ combine.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import time
+import pandas as pd
+import numpy as np
+
+from utils import readChunk, toCSV
+
+def combineMonth(data_dir, out_dir):
+	all_df = []
+	for f in sorted(os.listdir(data_dir)):
+		if f.endswith(".csv"):
+			df = readChunk(os.path.join(data_dir, f))
+			all_df.append(df)
+	all_df = pd.concat(all_df)
+	print(len(all_df))
+
+if __name__ == '__main__':
+	combineMonth("../events/december")
+	# main("../events/2019/01", "../events/january")
+	# main("../events/2019/02", "../events/february")
+	# main("../events/2019/03", "../events/march")
+	# main("../events/2019/04", "../events/april")
+	# main("../events/2019/05", "../events/may")
