76e4bebe1be84ec971c23d823a68a5b88b116c0d
ririgi <lrpbaltazar@gmail.com>
2019-08-22 17:39:29 +0800

    add option to check if logged in

--- combine.py
+++ combine.py
@@ -16,25 +16,26 @@ def removeNotLoggedIn(df):
 	df.drop('loggedin', axis = 1, inplace = True)
 	return df
 
-def combineMonth(data_dir, outfile):
+def combineMonth(data_dir, outfile, check_login = False):
 	all_df = []
 	for f in sorted(os.listdir(data_dir)):
 		if f.endswith(".csv"):
 			df = readChunk(os.path.join(data_dir, f))
 			df.dropna(subset = ['USERID'], inplace = True)
-			df.USERID = df.USERID.astype(str)
-			df.PRIMARY_FINGERPRINT = df.PRIMARY_FINGERPRINT.astype(str)
-			df = removeNotLoggedIn(df)
+			if check_login:
+				df.USERID = df.USERID.astype(str)
+				df.PRIMARY_FINGERPRINT = df.PRIMARY_FINGERPRINT.astype(str)
+				df = removeNotLoggedIn(df)
 			all_df.append(df)
 	all_df = pd.concat(all_df)
 	toCSV(all_df, outfile, index = False)
 
 if __name__ == '__main__':
-	combineMonth("../events/december", "results/december_customers.csv")
-	combineMonth("../events/january", "results/january_customers.csv")
-	combineMonth("../events/february", "results/february_customers.csv")
-	combineMonth("../events/march", "results/march_customers.csv")
-	combineMonth("../events/april", "results/april_customers.csv")
-	combineMonth("../events/may", "results/may_customers.csv")
+	# combineMonth("../events/december", "results/december_customers.csv")
+	# combineMonth("../events/january", "results/january_customers.csv")
+	# combineMonth("../events/february", "results/february_customers.csv")
+	# combineMonth("../events/march", "results/march_customers.csv")
+	# combineMonth("../events/april", "results/april_customers.csv")
+	# combineMonth("../events/may", "results/may_customers.csv")
 
-	combineMonth("results", "results/all_month.csv")
+	combineMonth("results", "all_month.csv")
