afed68a41d5209724e71cb95d2552613b1ef01d8
ririgi <lrpbaltazar@gmail.com>
2019-08-22 18:02:50 +0800

    use all_month instead of query data

--- prepare.py
+++ prepare.py
@@ -6,7 +6,9 @@ import time
 import pandas as pd
 import numpy as np
 
-from utils import readChunk
+from utils import readChunk, toCSV
+import matplotlib as mpl
+mpl.use('tKagg')
 from matplotlib import pyplot as plt
 import seaborn as sns
 
@@ -16,10 +18,10 @@ sns.set()
 style.use('seaborn-poster')
 style.use('bmh')
 
-file = "content_regularity.csv"
+file = "all_month.csv"
 
-df = readChunk(file, header = None)
-df.rename(columns = {0:'USERID', 1:'SESSION_ID', 2:'CONTENT_TYPE', 3:'PRIMARY_FINGERPRINT', 4:'SESSION_STARTDT_YEAR', 5:'SESSION_STARTDT_MONTH', 6:'SESSION_STARTDT_DAY', 7:'HOUR'}, inplace = True)
+df = readChunk(file)
+# df.rename(columns = {0:'USERID', 1:'SESSION_ID', 2:'CONTENT_TYPE', 3:'PRIMARY_FINGERPRINT', 4:'SESSION_STARTDT_YEAR', 5:'SESSION_STARTDT_MONTH', 6:'SESSION_STARTDT_DAY', 7:'HOUR'}, inplace = True)
 content_type = {'ABS-CBN - Show - Current': 1, 'ABS-CBN - Show - Old':2, 'Original Show':3, 'Original Movie':4, 'Movie':5, 'Live':6, 'Fast Cut':7, 'Other - Show':8, 'Preview':9, 'Trailer':10}
 
 df.CONTENT_TYPE = df.CONTENT_TYPE.astype(str)
@@ -44,17 +46,15 @@ def getCustomerDay(month, date):
 		return date+31+31+28+31
 	else:
 		return date+31+31+28+31+30
+
 df['DAY'] = df[['SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY']].apply(lambda x: getCustomerDay(x[0], x[1]), axis = 1)
 
-count = 0
 for user in df.USERID.unique():
-	if count == 3: break
+	if count == 10: break
 	temp = df.loc[df.USERID == user]
 	new_df = pd.DataFrame(index = list(range(0, 24)), columns = list(range(1, 183)))
 	for i in temp.index.unique():
 		new_df.iloc[temp.loc[i]['HOUR']][temp.loc[i]['DAY']] = temp.loc[i]['CONTENT_TYPE']
+	new_df.index.name = 'HOUR'
 	new_df.fillna(0, inplace = True)
-	print(new_df.head(24))
-	plot = sns.heatmap(new_df, fmt="d")
-	plt.show()
-	count = count+1
+	toCSV(new_df, "visualization/"+user+".csv")
