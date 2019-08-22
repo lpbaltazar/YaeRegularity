30ab191bb5d9f5ad7ceb692e1487c8dfb748ed28
ririgi <lrpbaltazar@gmail.com>
2019-08-22 11:27:55 +0800

    add code for utils and extraction of column of interest

---
+++ extract.py
@@ -0,0 +1,25 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import time
+import pandas as pd
+import numpy as np
+
+from utils import readChunk, toCSV
+
+def main(data_dir, out_dir):
+	for f in sorted(os.listdir(data_dir)):
+		if f.endswith(".csv"):
+			df = readChunk(os.path.join(data_dir, f))
+			df = df[['USERID', 'SESSIONID', 'PRIMARY_FINGERPRINT', 'CONTENT_TYPE', 'SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY', 'SESSION_STARTDT']]
+			s = time.time()
+			df['SESSION_STARTDT'] = pd.to_datetime(df['SESSION_STARTDT'])
+			df['HOUR'] = df.SESSION_STARTDT.dt.hour
+			e = time.time()
+			total_time = time.strftime("%H:%M:%S", time.gmtime(e-s))
+			print("Finish getting hour in {}".format(total_time))
+			toCSV(df, os.path.join(out_dir, f), index = False)
+
+if __name__ == '__main__':
+	main("../events/2018/12", "../events/december")
