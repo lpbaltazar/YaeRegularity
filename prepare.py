18e97807d9c1a3ea7bd40f41945053006b6cf26c
ririgi <lrpbaltazar@gmail.com>
2019-08-28 09:37:31 +0800

    prepare data for champions

--- prepare.py
+++ prepare.py
@@ -19,7 +19,8 @@ style.use('seaborn-poster')
 style.use('bmh')
 
 file = "all_month.csv"
-
+users = ["0013292CF87E70A73CF297C5D153C223", "008C58D802A70B19D6213C4860508135", "025A6C92BF0E4510972E82D220FBEACA", "05EF69C6197E4C17990A12655C4C07DC", "6DE572EE144640C1BB76CFC3CFAA3036"]
+outdir = "visualization/champions"
 df = readChunk(file)
 # df.rename(columns = {0:'USERID', 1:'SESSION_ID', 2:'CONTENT_TYPE', 3:'PRIMARY_FINGERPRINT', 4:'SESSION_STARTDT_YEAR', 5:'SESSION_STARTDT_MONTH', 6:'SESSION_STARTDT_DAY', 7:'HOUR'}, inplace = True)
 
@@ -29,11 +30,11 @@ df.HOUR = df.HOUR.astype(int)
 
 
 for user in df.USERID.unique():
-	if user != '072A34C9427A4C15B0ED31FE6BEC48D8': continue
-	temp = df.loc[df.USERID == user]
-	new_df = pd.DataFrame(index = list(range(0, 24)), columns = list(range(1, 183)))
-	for i in temp.index.unique():
-		new_df.iloc[temp.loc[i]['HOUR']][temp.loc[i]['DAY']] = temp.loc[i]['CONTENT_TYPE']
-	new_df.index.name = 'HOUR'
-	new_df.fillna(0, inplace = True)
-	toCSV(new_df, "visualization/"+user+".csv")
+	if user in users:
+		temp = df.loc[df.USERID == user]
+		new_df = pd.DataFrame(index = list(range(0, 24)), columns = list(range(1, 183)))
+		for i in temp.index.unique():
+			new_df.iloc[temp.loc[i]['HOUR']][temp.loc[i]['DAY']] = temp.loc[i]['CONTENT_TYPE']
+		new_df.index.name = 'HOUR'
+		new_df.fillna(0, inplace = True)
+		toCSV(new_df, outdir+user+".csv")
