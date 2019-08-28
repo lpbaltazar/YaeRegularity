7da45f54ac50a4b7effa793777325f19fdca26d0
ririgi <lrpbaltazar@gmail.com>
2019-08-28 10:26:41 +0800

    make plotting of heatmap a function

--- plot.py
+++ plot.py
@@ -21,21 +21,22 @@ style.use('bmh')
 file = '../data/072A34C9427A4C15B0ED31FE6BEC48D8.csv'
 data_dir = 'visualization/champs'
 
-for f in sorted(os.listdir(data_dir)):
-	if f.endswith(".csv"):
-		df = pd.read_csv(file)
-		plt.figure(figsize = (20, 9))
-		df.replace(hour, inplace = True)
-		df.set_index('HOUR', inplace = True)
-		df.replace(0, np.nan, inplace = True)
-		plot = sns.heatmap(df, annot = True, cbar = False, annot_kws={"fontsize":7}, vmin = 0, vmax = 10)
-		plot.set_xlabel('DAYS')
-		plot.set_yticklabels(['12MN','1AM','2AM','3AM','4AM','5AM','6AM','7AM','8AM','9AM','10AM','11AM','12NN','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM'])
-		plot.set_ylabel('TIME')
-		plt.yticks(fontsize=10, rotation=360)
-		plt.xticks(fontsize=8)
-		plt.vlines([31, 62, 90, 121, 151], *plot.get_ylim())
-		plt.savefig(data_dir+"/figures"+f[:-4]+".png")
+def plotHeatmap(data_dir):
+	for f in sorted(os.listdir(data_dir)):
+		if f.endswith(".csv"):
+			df = pd.read_csv(file)
+			plt.figure(figsize = (20, 9))
+			df.replace(hour, inplace = True)
+			df.set_index('HOUR', inplace = True)
+			df.replace(0, np.nan, inplace = True)
+			plot = sns.heatmap(df, annot = True, cbar = False, annot_kws={"fontsize":7}, vmin = 0, vmax = 10)
+			plot.set_xlabel('DAYS')
+			plot.set_yticklabels(['12MN','1AM','2AM','3AM','4AM','5AM','6AM','7AM','8AM','9AM','10AM','11AM','12NN','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM'])
+			plot.set_ylabel('TIME')
+			plt.yticks(fontsize=10, rotation=360)
+			plt.xticks(fontsize=8)
+			plt.vlines([31, 62, 90, 121, 151], *plot.get_ylim())
+			plt.savefig(data_dir+"/figures"+f[:-4]+".png")
 
 # unique = []
 # for i in df.columns:
@@ -47,3 +48,11 @@ for f in sorted(os.listdir(data_dir)):
 # 		temp = df.loc[df[j] == i]
 # 		value = value + len(temp)
 # 	print(i, value)
+
+if __name__ == '__main__':
+	plotHeatmap("visualization/champs")
+	plotHeatmap("visualization/loyal")
+	plotHeatmap("visualization/promising")
+	plotHeatmap("visualization/binge")
+	plotHeatmap("visualization/atrisk")
+	plotHeatmap("visualization/lost")
