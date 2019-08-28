efb90249fe382158ed00d91dd0af78b79c64e47a
ririgi <lrpbaltazar@gmail.com>
2019-08-28 11:36:53 +0800

    change colormap and add dpi argument to savefig

--- plot.py
+++ plot.py
@@ -9,6 +9,7 @@ import numpy as np
 from utils import readChunk, toCSV
 # import matplotlib as mpl
 # mpl.use('tKagg')
+# mpl.rcParams['figure.dpi'] = 300
 from matplotlib import pyplot as plt
 import seaborn as sns
 
@@ -25,7 +26,7 @@ def plotHeatmap(data_dir):
 			# plt.figure(figsize = (20, 9))
 			df.set_index('HOUR', inplace = True)
 			df.replace(0, np.nan, inplace = True)
-			plot = sns.heatmap(df, annot = True, cbar = False, annot_kws={"fontsize":5}, vmin = 0, vmax = 10, cmap = 'Pastel2')
+			plot = sns.heatmap(df, annot = True, cbar = False, annot_kws={"fontsize":5}, vmin = 1, vmax = 10, cmap = 'Set3')
 			plot.set_xlabel('DAYS')
 			plot.set_yticklabels(['12MN','1AM','2AM','3AM','4AM','5AM','6AM','7AM','8AM','9AM','10AM','11AM','12NN','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM'])
 			plot.set_ylabel('TIME')
@@ -33,7 +34,8 @@ def plotHeatmap(data_dir):
 			plt.xticks(fontsize=8)
 			plt.vlines([31, 62, 90, 121, 151], *plot.get_ylim())
 			plt.tight_layout()
-			plt.savefig(data_dir+"/figures/"+f[:-4]+".png")
+			plt.savefig(data_dir+"/figures/"+f[:-4]+".png", dpi = 300)
+			# plt.show()
 			plt.clf()
 
 # unique = []
