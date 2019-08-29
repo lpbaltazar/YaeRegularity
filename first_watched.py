70997aa62fedb27ea22f8adc05f8f2545847c59a
ririgi <lrpbaltazar@gmail.com>
2019-08-29 10:28:56 +0800

    fix errors

--- first_watched.py
+++ first_watched.py
@@ -9,36 +9,36 @@ import numpy as np
 from utils import readChunk, toCSV
 
 file = "all_month.csv"
-usecols = ['USERID', 'CONTENT_TYPE', 'VIDEO_CATEGORY_TITLE', 'SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY', 'STARTHOUR']
+usecols = ['USERID', 'CONTENT_TYPE', 'SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY', 'HOUR']
 df = readChunk(file, usecols = usecols)
 
 df = df.loc[df.SESSION_STARTDT_MONTH != '11']
 convert = {'12':'0'}
 df.replace({'SESSION_STARTDT_MONTH':convert}, inplace = True)
-toint = ['SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY', 'STARTHOUR']
+toint = ['SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY', 'HOUR']
 for i in toint:
 	df[i] = df[i].astype(int)
-df['ORDER'] = (df.SESSION_STARTDT_MONTH*100)+(df.SESSION_STARTDT_DAY*10)+df.STARTHOUR
+df['ORDER'] = (df.SESSION_STARTDT_MONTH*100)+(df.SESSION_STARTDT_DAY*10)
 
 df.sort_values('ORDER', inplace = True)
 
 print(len(df))
 df.drop_duplicates(subset = ['USERID'], keep = 'first', inplace = True)
 print(len(df))
+print(df.head(20))
+labels = pd.read_csv('clustering_6.csv')
+labels.columns = labels.columns.str.upper()
 
-# labels = pd.read_csv('clustering_6.csv')
-# labels.columns = labels.columns.str.upper()
+df = df.merge(labels, how = 'left', on = 'USERID')
 
-# df = df.merge(labels, how = 'left', on = 'USERID')
+for i in df.LABEL.unique():
+	print(i)
+	temp = df.loc[df.LABEL == i]
+	new_df = pd.DataFrame(index = list(range(1, 11)), columns = ['COUNT'])
+	for j in df.CONTENT_TYPE.unique():
+		temp2 = temp.loc[temp.CONTENT_TYPE == j]
+		new_df.loc[int(j)]['COUNT'] = len(temp2)
 
-# for i in df.LABEL:
-# 	print(df.label)
-# 	temp = df.loc[df.label == i]
-# 	new_df = pd.DataFrame(index = list(range(1, 11)), columns = ['COUNT'])
-# 	for j in df.CONTENT_TYPE:
-# 		temp2 = temp.loc[temp.CONTENT_TYPE == j]
-# 		new_df.loc[int(j)]['COUNT'] = len(temp2)
-
-# 	print(new_df)
-# 	new_df.index.name = 'CONTENT_TYPE'
-# 	toCSV(new_df, 'results/firstwatched/'+i+'.csv')
+	print(new_df)
+	new_df.index.name = 'CONTENT_TYPE'
+	toCSV(new_df, 'results/firstwatched/'+str(i)+'.csv')
