40d873130024073b532c35964f74fd63f82d65d4
ririgi <lrpbaltazar@gmail.com>
2019-08-29 11:06:22 +0800

    make barplot group

--- plot_first_watched.py
+++ plot_first_watched.py
@@ -7,8 +7,8 @@ import pandas as pd
 import numpy as np
 
 from utils import readChunk, toCSV
-# import matplotlib as mpl
-# mpl.use('tKagg')
+import matplotlib as mpl
+mpl.use('tKagg')
 from matplotlib import pyplot as plt
 import seaborn as sns
 
@@ -24,18 +24,17 @@ for f in sorted(os.listdir(data_dir)):
 	if f.endswith(".csv"):
 		df = pd.read_csv(os.path.join(data_dir, f))
 		# df.CONTENT_TYPE = df.CONTENT_TYPE.astype(str)
-		plot = sns.barplot(x = 'COUNT', y = 'CONTENT_TYPE', data = df, orient = 'h', palette = 'Set3')
+		df.set_index('CONTENT_TYPE', inplace = True)
+		plot = df.T.plot(kind = 'bar', colormap = 'Set3', rot = 0)
 		# plot = df.plot(kind = 'bar', colormap = 'Set3')
-		tot = df.COUNT.sum()
-		df['percent'] = (df['COUNT']/tot)*100
-		df['percent'] = df['percent'].astype(float)
-		df['percent'] = round(df['percent'], 1)
-		for p in plot.patches:
-			width = p.get_width()
-			plt.text(p.get_width(), p.get_y()+0.55*p.get_height(), '{:1.0f}'.format(width), va = 'center')
+		# for p in plot.patches:
+		# 	width = p.get_width()
+		# 	plt.text(p.get_width(), p.get_y()+0.55*p.get_height(), '{:1.0f}'.format(width), va = 'center')
 		# for i in range(df.shape[0]):
 		# 	plot.text(i, df.iloc[i]['COUNT'], str(df.iloc[i]['percent']), horizontalalignment = 'center')
-		plot.set_yticklabels(['ABS-CBN - Show - Current', 'ABS-CBN - Show - Old', 'Original Show', 'Original Movie', 'Movie', 'Live', 'Fast Cut', 'Other - Show', 'Preview', 'Trailer'])
-		plot.set_ylabel('')
+		plot.legend(['ABS-CBN - Show - Current', 'ABS-CBN - Show - Old', 'Original Show', 'Original Movie', 'Movie', 'Live', 'Fast Cut', 'Other - Show', 'Preview', 'Trailer'])
+		plot.set_xticklabels(['DECEMBER', 'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY'])
+		# plot.set_ylabel('')
 		plt.tight_layout()
+		# plt.show()
 		plt.savefig(data_dir+f[:-4]+'.png', dpi = 600)
