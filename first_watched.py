b9af2b539b09d5f86868747e809d45ac10b4678b
ririgi <lrpbaltazar@gmail.com>
2019-08-29 08:49:53 +0800

    add labels to customers

--- first_watched.py
+++ first_watched.py
@@ -6,7 +6,7 @@ import time
 import pandas as pd
 import numpy as np
 
-from utils import readChunk
+from utils import readChunk, toCSV
 
 file = "all_month.csv"
 df = readChunk(file)
@@ -14,9 +14,8 @@ df = readChunk(file)
 to_int = ['SESSION_STARTDT_MONTH', 'SESSION_STARTDT_DAY', 'STARTHOUR']
 
 
-df = df.loc[df.SESSION_STARTDT_MONTH != '11']
-df['SESSION_STARTDT_MONTH'] = df['SESSION_STARTDT_MONTH'].apply(lambda x: '0' if x == '12' else x)
-
+convert = {'12':'0'}
+df.replace({'SESSION_STARTDT_MONTH':convert}, inplace = True)
 df['ORDER'] = df.SESSION_STARTDT_MONTH+df.SESSION_STARTDT_DAY+df.STARTHOUR
 df.ORDER = df.ORDER.astype(int)
 
@@ -25,3 +24,20 @@ df.sort_values('ORDER', inplace = True)
 print(len(df))
 df.drop_duplicates(subset = ['USERID'], keep = 'first', inplace = True)
 print(len(df))
+
+labels = pd.read_csv('clustering_6.csv')
+labels.columns = labels.columns.str.upper()
+
+df = df.merge(labels, how = 'left', on = 'USERID')
+
+for i in df.LABEL:
+	print(df.label)
+	temp = df.loc[df.label == i]
+	new_df = pd.DataFrame(index = list(range(1, 11)), columns = ['COUNT'])
+	for j in df.CONTENT_TYPE:
+		temp2 = temp.loc[temp.CONTENT_TYPE == j]
+		new_df.loc[int(j)]['COUNT'] = len(temp2)
+
+	print(new_df)
+	new_df.index.name = 'CONTENT_TYPE'
+	toCSV(new_df, 'results/firstwatched/'+i+'.csv')
